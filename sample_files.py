e22bf8e38cd79f203b92a5e36c0b4783e8dcd4cf
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2026-04-30 15:50:31 +0200

    single channel RX: add on-air samples
    This has been recorded with two devices at a distance of about 2.5m and
    additional 10 dB attenuation on DUT side.

--- sample_files.py
+++ sample_files.py
@@ -8,7 +8,8 @@
 fileinfo = {
     # filename: description, sample_rate, frequency_offset, channel_spacing, symbol_rate, expected_packet_lengths
     'samples/single_channel/ping_1Msps_863MHz_50ksps_channel0.cfile': (
-        "5 pings from a device to a router. Each ping consists of ping request, ACK, ping response, ACK. Device and router were connected via a RF power splitter with a total attenuation of 56 dB.",
+        "5 pings from a device to a router. Each ping consists of ping request, ACK, ping response, ACK. "
+        "Device and router were connected via a RF power splitter with a total attenuation of 56 dB.",
         1000000,
         100000,
         100000,
@@ -24,5 +25,15 @@ fileinfo = {
         200000,
         100000,
         [168, 48, 154, 48] * 5 + [169]
-    )
+    ),
+    'samples/single_channel/ping_1Msps_863MHz_100ksps_channel0_on_air.cfile': (
+        "5 pings from a device to a router. Each ping consists of ping request, ACK, ping response, ACK. "
+        "Devices connected using antennas. DUT has 10 dB additional attenuation. "
+        "Additionally contains a PAN advertisement as last packet.",
+        1000000,
+        100000,
+        200000,
+        100000,
+        [168, 48, 154, 48] * 5 + [70]
+    ),
 }
